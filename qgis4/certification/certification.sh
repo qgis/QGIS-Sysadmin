cb208810e597764f4b784c9d867f44a776d2247c
Blottiere Paul <blottiere.paul@gmail.com>
2019-04-11 10:03:01 +0200

    Add certification script

---
+++ qgis4/certification/certification.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# download data
+URL=http://cite.opengeospatial.org/teamengine/about/wms/1.3.0/site/
+
+if [ ! -f data/shapefile/Autos.shp ]
+then
+  cd data
+  wget $URL/data-wms-1.3.0.zip
+  unzip data-wms-1.3.0.zip
+  rm data-wms-1.3.0.zip
+  cd -
+fi
+
+# build 3.4 branch
+cd ../docker/3.4 && sh build.sh && cd -
+
+# stop and restart
+docker-compose down
+docker-compose rm -f
+docker-compose up -d
