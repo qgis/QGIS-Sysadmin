b3ec9163195971f5e62b688e33366f3ef1857647
Blottiere Paul <blottiere.paul@gmail.com>
2019-04-23 15:02:11 +0200

    Push aggregate report on qgis-test

--- qgis4/perfsuite-aggregate/aggregate-ci.sh
+++ qgis4/perfsuite-aggregate/aggregate-ci.sh
@@ -15,3 +15,7 @@ fi
 rm -rf /tmp/graffiti_aggregate/
 . graffiti/venv/bin/activate
 python aggregate.py
+
+scp /tmp/graffiti_aggregate/aggregate.html qgis-test:/var/www/qgisdata/QGIS-tests/perf_test/graffiti/aggregate/
+scp /tmp/graffiti_aggregate/style.css qgis-test:/var/www/qgisdata/QGIS-tests/perf_test/graffiti/aggregate/
+scp -r /tmp/graffiti_aggregate/graph qgis-test:/var/www/qgisdata/QGIS-tests/perf_test/graffiti/aggregate/
