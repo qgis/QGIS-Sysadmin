4940c5b34afae7e6a3115b853c9c77ee7fe4d37d
Blottiere Paul <blottiere.paul@gmail.com>
2019-06-19 10:13:31 +0200

    Keep legend order and add style

--- qgis4/perfsuite-aggregate/aggregate.py
+++ qgis4/perfsuite-aggregate/aggregate.py
@@ -1,45 +1,52 @@
 from tqdm import trange
+from collections import OrderedDict
 
 from graffiti.graffiti.request import Request
 from graffiti.graffiti.graph import Graph
 from graffiti.graffiti.report import Report
 from graffiti.graffiti.config import Config
 from graffiti.graffiti.database import Database
+from graffiti.graffiti.style import Style
 
-cfg = Config('scenarios.yml')
+cfg = Config('scenarios.yml', 'style.yml')
 
 database = Database(cfg.database)
-
+style = Style(cfg.styles)
 report = Report(cfg.title, cfg.date, cfg.logo)
 
 for i in trange(len(cfg.requests), desc='Requests'):
     req = Request.build(cfg.requests[i])
 
     means = database.means(req, limit=30, min=0.05)
-    durations = {}
+    durations = OrderedDict()
 
     days = None
     dates = None
-    for host in means:
-        host_means = []
-        for value in means[host]:
-            host_means.append(value[1])
-        durations[host] = host_means
 
-        if not dates:
-            dates = []
-            for value in means[host]:
-                dates.append(value[0].split(' ')[0])
+    for host in req.hosts:
+        for mean_host in means:
+            if mean_host != host.name:
+                continue
 
-        if not days:
-            days = len(host_means)
+            host_means = []
+            for value in means[mean_host]:
+                host_means.append(value[1])
+            durations[mean_host] = host_means
+
+            if not dates:
+                dates = []
+                for value in means[mean_host]:
+                    dates.append(value[0].split(' ')[0])
+
+            if not days:
+                days = len(host_means)
 
     req.durations = durations
 
     x_labels = dates
 
     x_title = '{} days'.format(days)
-    graph = Graph(req)
+    graph = Graph(req, style)
     graph.draw_temporal(cfg.imdir, x_title=x_title, x_labels=x_labels, x_label_rotation=45)
     graph.draw_box(cfg.imdir, x_title=x_title)
     report.add(graph)
