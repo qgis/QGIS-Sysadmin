0e2ff09a2b637717c640cd38fe38e9afd415a049
Blottiere Paul <blottiere.paul@gmail.com>
2020-10-05 15:14:33 +0200

    Upgrade qgis server instances in perfsuite

--- qgis4/ansible/roles/perfsuite/files/perfsuite-ci.sh
+++ qgis4/ansible/roles/perfsuite/files/perfsuite-ci.sh
@@ -5,15 +5,15 @@ DATE=$(date +"%Y_%m_%d_%H_%M")
 echo "Remove docker images"
 echo "--------------------"
 docker rmi qgisserver-perfsuite/2.18
-docker rmi qgisserver-perfsuite/3.4
-docker rmi qgisserver-perfsuite/3.6
+docker rmi qgisserver-perfsuite/3.10
+docker rmi qgisserver-perfsuite/3.14
 docker rmi qgisserver-perfsuite/master
 
 echo "Build new docker images"
 echo "-----------------------"
 cd QGIS-Server-PerfSuite/docker/2.18 && sh build.sh && cd -
-cd QGIS-Server-PerfSuite/docker/3.4 && sh build.sh && cd -
-cd QGIS-Server-PerfSuite/docker/3.6 && sh build.sh && cd -
+cd QGIS-Server-PerfSuite/docker/3.10 && sh build.sh && cd -
+cd QGIS-Server-PerfSuite/docker/3.14 && sh build.sh && cd -
 cd QGIS-Server-PerfSuite/docker/master && sh build.sh && cd -
 
 echo "Run graffiti"
